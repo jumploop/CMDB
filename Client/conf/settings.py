4b695479a12b5c721ab9811f84efb02f59ec00fd
liming <827182486@qq.com>
2020-07-30 22:03:11 +0800

    主功能模块;配置文件;信息收集模块

---
+++ Client/conf/settings.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/30 21:53
+# @Author  : 一叶知秋
+# @File    : settings.py
+# @Software: PyCharm
+import os
+
+# 远端接收数据的服务器
+Params = {
+    "server": "192.168.0.100",
+    "port": 8000,
+    'url': '/assets/report/',
+    'request_timeout': 30,
+}
+
+# 日志文件配置
+
+PATH = os.path.join(os.path.dirname(os.getcwd()), 'log', 'cmdb.log')
